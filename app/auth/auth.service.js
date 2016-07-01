a22b12e35184ee15d71630923c27e01fd1bf2d82
technetos <jogould@umail.iu.edu>
2016-07-01 11:28:45 -0400

    removed all firebase auth handling and replace the auth handling with functions to handle communicating with backend

--- app/auth/auth.service.js
+++ app/auth/auth.service.js
@@ -5,18 +5,16 @@
     .module('mutantApp.auth')
     .factory('authService', authService);
 
-  authService.$inject = ['$firebaseAuth', 'firebaseDataService', 'mutantService'];
+  authService.$inject = ['$http', '$window'];
 
-  function authService($firebaseAuth, firebaseDataService, mutantService) {
-    var auth = $firebaseAuth();
+  function authService($http, $window) {
 
     var service = {
       register: register,
       login: login,
       logout: logout,
       isLoggedIn: isLoggedIn,
-      auth: auth,
-      sendWelcomeEmail: sendWelcomeEmail,
+      //sendWelcomeEmail: sendWelcomeEmail,
     };
 
     return service;
@@ -24,27 +22,43 @@
     ///////////
 
     function register(user) {
-      return auth.$createUserWithEmailAndPassword(user.email, user.password);
+        return $http.post('/api/register', user)
+            .success(function(data) {
+                // Save the token to local storage
+                $window.localStorage['mutant-token'] = data.token;
+            });
     }
 
     function login(user) {
-      return auth.$signInWithEmailAndPassword(user.email, user.password);
+        return $http.post('/api/login', user)
+            .success(function(data) {
+                // Save the token to local storage
+                $window.localStorage['mutant-token'] = data.token;
     }
 
     function logout() {
-      mutantService.reset();
-      auth.$signOut();
+        // Remove the token from local storage
+        $window.localStorage.removeItem('mutant-token');
     }
 
     function isLoggedIn() {
-      return auth.$getAuth();
+        var token = $window.localStorage['mutant-token'];
+        var payload;
+
+        if(token) {
+            payload = token.split('.')[1];
+            payload = $window.atob(payload);
+            payload = JSON.parse(payload);
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
     }
 
-    function sendWelcomeEmail(emailAddress) {
+    /*function sendWelcomeEmail(emailAddress) {
       firebaseDataService.emails.push({
         emailAddress: emailAddress
       });
-    }
-
+    }*/
   }
 })();
